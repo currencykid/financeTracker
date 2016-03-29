5dfb6b111f255de3a69bdbe61ec9f1245cd10b41
ck <thecurrencykid@gmail.com>
2016-03-28 21:34:11 -0700

    stocks and ajax for UI added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,5 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
+//
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
@@ -14,4 +15,41 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
-//= require_tree .
+//= require_tree . 
+
+var hide_spinner = function(){
+	$('#spinner').hide();
+}
+
+var show_spinner= function(){
+	$('#spinner').show(); 
+}
+
+var init_stock_lookup;
+
+
+init_stock_lookup = function() {
+	$('#stock-lookup-form').on('ajax:before', function(event,data,status){
+		show_spinner();
+	});
+
+	$('#stock-lookup-form').on('ajax:after', function(event,data,status){
+		hide_spinner();
+	});
+
+	$('#stock-lookup-form').on('ajax:success', function(event, data, status) {
+		$('#stock-lookup').replaceWith(data);
+		init_stock_lookup();
+	});
+	$('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error) {
+		hide_spinner(); 
+		$('#stock-lookup-results').replaceWith(' ');
+		$('#stock-lookup-errors').replaceWith('Stock was not found');
+	});
+};
+
+$(document).ready(function() {
+	init_stock_lookup();
+});
+
+
